064292d122e428316e07d732ca3cf9377399fb2d
guozhaohui <guozhaohui628@gmail.com>
2018-01-09 15:02:47 +0800

    xxx

--- app/src/main/java/guo/mydashboardviwe/view/MyDashBoardView.java
+++ app/src/main/java/guo/mydashboardviwe/view/MyDashBoardView.java
@@ -1,24 +1,20 @@
 package guo.mydashboardviwe.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.os.Build;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+
 
 import java.text.DecimalFormat;
 
